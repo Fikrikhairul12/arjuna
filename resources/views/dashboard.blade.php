68a10b4e49f29c8c6e9332a076cd85c6bbdf25b9
Fikrikhairul12 <khairulfikri1289@gmail.com>
2026-04-15 20:49:14 +0700

    bnrin dikit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
-<div class="container w-full h-100 bg-linear-to-r from-blue-500 via-sky-400 to-cyan-300">
-
+<div class="container w-full h-100 bg-linear-to-r from-blue-500 to-blue-700">
+    <h1 class="text-6xl font-bold font-mono">KERIPIK TEMPE ARJUNA</h1>
+    <P></P>
 </div>
--- resources/views/navbar/footer.blade.php
+++ resources/views/navbar/footer.blade.php
@@ -5,3 +5,5 @@
         </div>
     </div>
 </div>
+</body>
+</html>
